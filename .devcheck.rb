a4353318c2e713d84ccf9520950e14e135da1c39
Louie Bloomberg <louie@lnbloomberg.net>
2026-09-22 18:38:58 -0500

    added code to manage the commands and files checked by devcheck.rb

---
+++ .devcheck.rb
@@ -0,0 +1,15 @@
+# .devcheck.rb
+# devcheck
+# Copyright (c) 2026 Louie Bloomberg.
+# SPDX-License-Identifier: MIT
+
+
+# frozen_string_literal: true
+
+
+command "ruby"
+command "irb"
+command "git"
+
+file "LICENSE"
+file "README.md"
